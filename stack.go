3cba252945176fe2e0ab44445143068e793a7245
Dimitri Sokolyuk <demon@dim13.org>
2018-01-30 10:08:56 +0100

    kiss

--- stack.go
+++ stack.go
@@ -1,24 +1,22 @@
 package j1
 
-const stackSize = 0x20
-
 type stack struct {
-	data [stackSize]uint16 // stack
-	sp   int8              // 5 bit stack pointer
+	data [0x20]uint16 // stack
+	sp   int8         // 5 bit stack pointer
 }
 
 func (s *stack) move(dir int8) {
-	s.sp = (s.sp + dir) & (stackSize - 1)
+	s.sp = (s.sp + dir) & int8(len(s.data)-1)
 }
 
 func (s *stack) push(v uint16) {
-	s.sp = (s.sp + 1) & (stackSize - 1)
+	s.sp = (s.sp + 1) & int8(len(s.data)-1)
 	s.data[s.sp] = v
 }
 
 func (s *stack) pop() uint16 {
 	sp := s.sp
-	s.sp = (s.sp - 1) & (stackSize - 1)
+	s.sp = (s.sp - 1) & int8(len(s.data)-1)
 	return s.data[sp]
 }
 
