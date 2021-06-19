22b705bf39336e75e729c2c6081f6f677ded7a96
Dimitri Sokolyuk <sokolyuk@gmail.com>
2021-06-19 15:23:06 +0200

    Kiss

--- console/console.go
+++ console/console.go
@@ -8,59 +8,54 @@ import (
 
 // Console I/O
 type Console struct {
-	in, out chan uint16
-	ctx     context.Context
-	cancel  func()
+	input  chan uint16
+	cancel func()
 }
 
 // New console
 func New(ctx context.Context) (context.Context, *Console) {
+	ctx, cancel := context.WithCancel(ctx)
 	c := &Console{
-		in:  make(chan uint16, 1),
-		out: make(chan uint16, 1),
+		input:  make(chan uint16, 1),
+		cancel: cancel,
 	}
-	ctx, c.cancel = context.WithCancel(ctx)
-	go c.write(ctx)
-	go c.read(ctx)
+	go func() {
+		var v uint16
+		defer close(c.input)
+		for {
+			_, err := fmt.Scanf("%c", &v)
+			if err == io.EOF {
+				cancel()
+			}
+			select {
+			case <-ctx.Done():
+				return
+			case c.input <- v:
+			}
+		}
+	}()
 	return ctx, c
 }
 
-func (c *Console) read(ctx context.Context) {
-	defer close(c.in)
-	for {
-		var v uint16
-		_, err := fmt.Scanf("%c", &v)
-		if err == io.EOF {
-			c.Stop()
-		}
-		select {
-		case <-ctx.Done():
-			return
-		case c.in <- v:
-		}
-	}
-}
-
-func (c *Console) write(ctx context.Context) {
-	defer close(c.out)
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case v := <-c.out:
-			fmt.Printf("%c", v)
-		}
-	}
-}
-
 // Read from console
-func (c *Console) Read() uint16 { return <-c.in }
+func (c *Console) Read() uint16 {
+	return <-c.input
+}
 
 // Write to console
-func (c *Console) Write(v uint16) { c.out <- v }
+func (c *Console) Write(v uint16) {
+	fmt.Printf("%c", v)
+}
 
 // Len of input buffer
-func (c *Console) Len() uint16 { return uint16(len(c.in)) }
+func (c *Console) Len() uint16 {
+	if len(c.input) > 0 {
+		return 1
+	}
+	return 0
+}
 
 // Stop console
-func (c *Console) Stop() { c.cancel() }
+func (c *Console) Stop() {
+	c.cancel()
+}
